2ffa235f8e9e42f11a36041bd34d26dfdb9790ea
Emil-Linnebjerg <emillinnebjerg@gmail.com>
2022-07-17 21:43:09 +0200

    Lot of changes

--- NeuralNet/Neural.py
+++ NeuralNet/Neural.py
@@ -4,23 +4,17 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 
 #the inital setup
-n_input, n_hidden, n_out, batch_size, learning_rate = 10, 20, 4, 7, 0.01
+n_input, n_hidden, n_out, learning_rate = 10, 20, 4, 0.01
 accDataAmount = 100
 debug = 1
 pathHere = 'C:/My programs/Git/UnoAI/NeuralNet/'
 
 #input array
-data_x = torch.randn(batch_size, n_input)
 data_inputArrat = []
 data_inputArratAcc = []
 #output array
-data_y = (torch.rand(size=(batch_size, n_out)) < 0.5).float()
 data_outputArrat = []
 data_outputArratAcc = []
-
-if(debug):
-    print(data_x.size())
-    print(data_y.size())
 
 def loadData():
     file1 = open(pathHere + 'DataSaved.txt', 'r')
@@ -64,17 +58,13 @@ loadData()
 #create the network
 model = nn.Sequential(nn.Linear(n_input, n_hidden),
                       nn.Sigmoid(),
-                      nn.Linear(n_hidden, n_hidden-2),
+                      nn.Linear(n_hidden, n_hidden-4),
                       nn.Sigmoid(),
-                      nn.Linear(n_hidden-2, n_hidden-4),
+                      nn.Linear(n_hidden-4, n_hidden-8),
                       nn.Sigmoid(),
-                      nn.Linear(n_hidden-4, n_hidden-6),
+                      nn.Linear(n_hidden-8, n_hidden-12),
                       nn.Sigmoid(),
-                      nn.Linear(n_hidden-6, n_hidden-8),
-                      nn.Sigmoid(),
-                      nn.Linear(n_hidden-8, n_hidden-10),
-                      nn.Sigmoid(),
-                      nn.Linear(n_hidden-10, n_out),
+                      nn.Linear(n_hidden-12, n_out),
                       nn.Sigmoid())
 
 if(debug):
@@ -88,17 +78,18 @@ optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 losses = []
 for epoch in range(5000):
-    for key, value in enumerate(data_inputArrat):
-        pred_y = model(torch.tensor(value))
-        loss = loss_function(pred_y, torch.tensor(data_outputArrat[key]))
-        losses.append(loss.item())
-        print(loss)
-        print(epoch)
+    pred_y = model(torch.tensor(data_inputArrat))
+    loss = loss_function(pred_y, torch.tensor(data_outputArrat))
+    losses.append(loss.item())
+    print(loss)
+    print(epoch)
 
-        model.zero_grad()
-        loss.backward()
+    model.zero_grad()
+    loss.backward()
 
-        optimizer.step()
+    optimizer.step()
+    #for key, value in enumerate(data_inputArrat):
+        
     
 accuracyProp = 0
 
@@ -108,12 +99,16 @@ accuracyProp3 = 0
 accuracyProp4 = 0
 
 diviationProp1 = 0
+prop1A = 0
 diviations1 = []
 diviationProp2 = 0
+prop2A = 0
 diviations2 = []
 diviationProp3 = 0
+prop3A = 0
 diviations3 = []
 diviationProp4 = 0
+prop4A = 0
 diviations4 = []
 
 if(debug):
@@ -123,9 +118,13 @@ def sumDiviation(outP, num, key):
     if(0.5 < data_outputArratAcc[key][num]):
         if(0.5 > outP):
             return sumUpAccuracy(outP, num, key)
+        else:
+            return 0
     if(0.5 > data_outputArratAcc[key][num]):
         if(0.5 < outP):
             return sumUpAccuracy(outP, num, key)
+        else:
+            return 0
     
     return float(0.5)
         
@@ -136,42 +135,57 @@ def sumUpAccuracy(outP, num, key):
     else:
         return outP - data_outputArratAcc[key][num]
 
+def makeNumBinary(num):
+    if(num > 1):
+        return 1
+    if(num < 0):
+        return 0
+    return num
+
 for key, value in enumerate(data_inputArratAcc):
     pred_y = model(torch.tensor(value))
-    outP1 = torch.tensor(pred_y)[0]
-    outP2 = torch.tensor(pred_y)[1]
-    outP3 = torch.tensor(pred_y)[2]
-    outP4 = torch.tensor(pred_y)[3]
+    outP1 = makeNumBinary(torch.tensor(pred_y)[0])
+    outP2 = makeNumBinary(torch.tensor(pred_y)[1])
+    outP3 = makeNumBinary(torch.tensor(pred_y)[2])
+    outP4 = makeNumBinary(torch.tensor(pred_y)[3])
     
     accuracyProp1 += sumUpAccuracy(outP1, 0, key)
-    diviationProp1 += sumDiviation(outP1, 0, key)
-    diviations1.append(sumDiviation(outP1, 0, key))
+    if(0 != sumDiviation(outP1, 0, key)):
+        diviationProp1 += sumDiviation(outP1, 0, key)
+        diviations1.append(sumDiviation(outP1, 0, key))
+        prop1A += 1
     
     accuracyProp2 += sumUpAccuracy(outP2, 1, key)
-    diviationProp2 += sumDiviation(outP2, 1, key)
-    diviations2.append(sumDiviation(outP2, 1, key))
+    if(0 != sumDiviation(outP2, 1, key)):
+        diviationProp2 += sumDiviation(outP2, 1, key)
+        diviations2.append(sumDiviation(outP2, 1, key))
+        prop2A += 1
 
     accuracyProp3 += sumUpAccuracy(outP3, 2, key)
-    diviationProp3 += sumDiviation(outP3, 2, key)
-    diviations3.append(sumDiviation(outP3, 2, key))
+    if(0 != sumDiviation(outP3, 2, key)):
+        diviationProp3 += sumDiviation(outP3, 2, key)
+        diviations3.append(sumDiviation(outP3, 2, key))
+        prop3A += 1
 
     accuracyProp4 += sumUpAccuracy(outP4, 3, key)
-    diviationProp4 += sumDiviation(outP4, 3, key)
-    diviations4.append(sumDiviation(outP4, 3, key))
+    if(0 != sumDiviation(outP4, 3, key)):
+        diviationProp4 += sumDiviation(outP4, 3, key)
+        diviations4.append(sumDiviation(outP4, 3, key))
+        prop4A += 1
 
     accuracyProp = accuracyProp1 + accuracyProp2 + accuracyProp3 + accuracyProp4
 
 
-accuracyProp = (accuracyProp/(accDataAmount*4))
-accuracyProp1 = (accuracyProp1/(accDataAmount))
-accuracyProp2 = (accuracyProp2/(accDataAmount))
-accuracyProp3 = (accuracyProp3/(accDataAmount))
-accuracyProp4 = (accuracyProp4/(accDataAmount))
+accuracyProp = 1-(accuracyProp/(accDataAmount*4))
+accuracyProp1 = 1-(accuracyProp1/(accDataAmount))
+accuracyProp2 = 1-(accuracyProp2/(accDataAmount))
+accuracyProp3 = 1-(accuracyProp3/(accDataAmount))
+accuracyProp4 = 1-(accuracyProp4/(accDataAmount))
 
-diviationProp1 = (diviationProp1/(accDataAmount))
-diviationProp2 = (diviationProp2/(accDataAmount))
-diviationProp3 = (diviationProp3/(accDataAmount))
-diviationProp4 = (diviationProp4/(accDataAmount))
+diviationProp1 = (diviationProp1/(prop1A))
+diviationProp2 = (diviationProp2/(prop2A))
+diviationProp3 = (diviationProp3/(prop3A))
+diviationProp4 = (diviationProp4/(prop4A))
 
 #torch.save(model, "C:\My programs\Git\MyNameIsJeff");
 
@@ -180,24 +194,36 @@ if(debug):
     #print ("loss was: ")
     #print (float(accuracyProp))
     print ("accuracy was: " + str(float(accuracyProp)))
-    print ("en turn accuracy was: " + str(float(accuracyProp1)))
+    print ("AI turn accuracy was: " + str(float(accuracyProp1)))
     print(diviationProp1)
-    print ("ai turn accuracy was: " + str(float(accuracyProp2)))
+    print ("En turn accuracy was: " + str(float(accuracyProp2)))
     print(diviationProp2)
-    print ("ai win accuracy was: " + str(float(accuracyProp3)))
+    print ("AI win accuracy was: " + str(float(accuracyProp3)))
     print(diviationProp3)
-    print ("en win accuracy was: " + str(float(accuracyProp4)))
+    print ("En win accuracy was: " + str(float(accuracyProp4)))
     print(diviationProp4)
 
-    plt.plot(diviationProp3)
+    plt.plot(diviations1)
     plt.ylabel('diviation')
-    plt.xlabel('iterations')
+    plt.xlabel('miss predictions')
+    plt.title("Average diviation rate3: %f"%(diviationProp1))
+    plt.show()   
+
+    plt.plot(diviations2)
+    plt.ylabel('diviation')
+    plt.xlabel('miss predictions')
+    plt.title("Average diviation rate3: %f"%(diviationProp2))
+    plt.show()   
+
+    plt.plot(diviations3)
+    plt.ylabel('diviation')
+    plt.xlabel('miss predictions')
     plt.title("Average diviation rate3: %f"%(diviationProp3))
     plt.show()   
 
-    plt.plot(diviationProp4)
+    plt.plot(diviations4)
     plt.ylabel('diviation')
-    plt.xlabel('iterations')
+    plt.xlabel('miss predictions')
     plt.title("Average diviation rate4: %f"%(diviationProp4))
     plt.show()   
     
